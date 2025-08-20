d7d16ba26cbc84c44e4b6644c85c1d0d80955470
KarynaC-reate <k4ahcarvalho@gmail.com>
2025-08-20 11:45:45 -0300

    API - edukacode
    Criação do repositório edukacode

---
+++ src/test/java/br/com/edukacode/api/ApiApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.edukacode.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
